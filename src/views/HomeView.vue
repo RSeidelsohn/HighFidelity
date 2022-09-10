750100c3d4068ca319e0bcc7fc8c50cd8b2feeb4
Roman Seidelsohn <rseidelsohn@rbloeth.de>
2022-09-10 14:12:42 +0200

    feat: Fill home view with first content (WIP)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
-</template>
-
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+    import { defineComponent } from 'vue';
+    import SearchForm from '@/components/SearchForm.vue'; // @ is an alias to /src
 
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
+    export default defineComponent({
+        name: 'HomeView',
+        components: {
+            SearchForm,
+        },
+    });
 </script>
+
+<template>
+    <div class="home">
+        <h1>HighFidelity</h1>
+        <h2>Your music search engine</h2>
+
+        <SearchForm />
+    </div>
+</template>
