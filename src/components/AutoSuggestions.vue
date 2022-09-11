775e0aaaa9c849e6c3198261f344a337426e8466
Roman Seidelsohn <rseidelsohn@rbloeth.de>
2022-09-11 15:34:01 +0200

    feat: Add functionality to AutoSuggestions component

--- src/components/AutoSuggestions.vue
+++ src/components/AutoSuggestions.vue
@@ -1,34 +1,92 @@
 <script lang="ts">
     import { defineComponent } from 'vue';
 
+    import SuggestionItem from '@/components/SuggestionItem.vue';
+
+    interface SearchData {
+        wrapperType: string;
+        value: string;
+    }
+
     export default defineComponent({
-        name: 'AutoSuggesions',
-        data() {
-            return {};
+        name: 'AutoSuggestions',
+        components: {
+            SuggestionItem,
         },
-        props: {},
-        methods: {},
+        props: {
+            artists: {
+                type: Array,
+                default() {
+                    return [];
+                },
+            },
+            albums: {
+                type: Array,
+                default() {
+                    return [];
+                },
+            },
+            songs: {
+                type: Array,
+                default() {
+                    return [];
+                },
+            },
+        },
+        computed: {
+            canShowArtists(): boolean {
+                return Boolean(this.artists.length > 0);
+            },
+            canShowAlbums(): boolean {
+                return Boolean(this.albums.length > 0);
+            },
+            canShowSongs(): boolean {
+                return Boolean(this.songs.length > 0);
+            },
+            canShowSomething(): boolean {
+                return Boolean(this.canShowArtists || this.canShowAlbums || this.canShowSongs);
+            },
+        },
+        methods: {
+            doSearch(searchData: SearchData) {
+                alert(`Type: ${searchData.wrapperType}, value: ${searchData.value}`);
+            },
+        },
     });
 </script>
 
 <template>
-    <div></div>
+    <div v-show="canShowSomething" class="border rounded bg-slate-50 max-w-[600px] w-5/6 mx-auto mb-8 text-left p-1">
+        <div v-show="canShowArtists">
+            <h3>Artists:</h3>
+            <SuggestionItem
+                v-for="(item, index) in artists"
+                :key="`artist_${index}`"
+                :item="item"
+                @useForSearch="doSearch"
+            />
+        </div>
+
+        <div v-show="canShowAlbums">
+            <h3>Albums:</h3>
+            <SuggestionItem
+                v-for="(item, index) in albums"
+                :key="`album_${index}`"
+                :item="item"
+                @useForSearch="doSearch"
+            />
+        </div>
+
+        <div v-show="canShowSongs">
+            <h3>Songs:</h3>
+            <SuggestionItem
+                v-for="(item, index) in songs"
+                :key="`song_${index}`"
+                :item="item"
+                @useForSearch="doSearch"
+            />
+        </div>
+    </div>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-    h3 {
-        margin: 40px 0 0;
-    }
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-    a {
-        color: #42b983;
-    }
-</style>
+<style scoped lang="scss"></style>
