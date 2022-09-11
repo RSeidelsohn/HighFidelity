227040bcc1fe5f37e39e1d6e8f803b59082460a2
Roman Seidelsohn <rseidelsohn@rbloeth.de>
2022-09-11 15:31:05 +0200

    feat: Add basic styles

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,19 @@
 
 <style lang="scss">
     #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
+        font-family: 'Lato', sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5 {
+        font-family: 'Carter One', sans-serif;
     }
 
     nav {
